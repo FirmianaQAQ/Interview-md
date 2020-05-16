c926a2855ae1ab7807713bd1aade20f65b31641d
Firmiana <935170397@qq.com>
2020-05-16 12:38:10 +0800

    :art: SUANFA

---
+++ zzlc_test6.js
@@ -0,0 +1,30 @@
+/**
+ * @Author: Firmiana
+ * @Date: 2020-05-16 09:18:53
+ * @Desc: 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+ * @Desc: k 是一个正整数，它的值小于或等于链表的长度。
+ * @Desc: 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * @Url: https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
+ */
+
+/**
+* Definition for singly-linked list.
+* function ListNode(val) {
+*     this.val = val;
+*     this.next = null;
+* }
+*/
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+
+const reverseKGroup = (head, k) => {
+	console.log(1)
+	return true
+}
+
+const res = reverseKGroup()
+
+console.log(res)
